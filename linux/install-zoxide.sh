0244ffb129fa3e1c3165f47b6311272f1a88f3d8
Hamza Moiyadi <hamza.moiyadi@gmail.com>
2023-07-08 20:08:45 +0530

    updated install-zoxide script

--- linux/install-zoxide.sh
+++ linux/install-zoxide.sh
@@ -3,7 +3,7 @@
 # Get the binary from github and install the command
 echo 'Fetching Zoxide installer...'
 
-curlCMD="wget https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash"
+curlCMD="curl https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash"
 eval $curlCMD
 echo "\nZoxide installed!\nAdding init script to ZSH now...\n"
 
@@ -11,3 +11,7 @@ echo "\nZoxide installed!\nAdding init script to ZSH now...\n"
 
 echo 'eval "$(zoxide init zsh)"' >> ~/.zshrc
 echo "\nZoxide configured!\n"
+
+echo 'Deleteing `install.sh`...'
+rm ./install.sh
+echo 'Deleted `install.sh`!'
