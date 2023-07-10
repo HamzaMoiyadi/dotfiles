cfaeaa8234e2d43f3426e4329df7f00080a80df2
Hamza Moiyadi <hamza.moiyadi@gmail.com>
2023-07-11 02:42:44 +0530

    added config file for PowerShell

---
+++ windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,3 @@
+# Add the path wherever starship is installed in order to get starship working for non-admin PowerShell
+
+Invoke-Expression (& 'C:\Program Files\starship\bin\starship.exe' init powershell)
