ded21927b44a7dc155887de9387457a36a7d0f6e
Hamza Moiyadi <hamza.moiyadi@gmail.com>
2025-03-25 04:22:57 +0530

    Updated aliases to reflect current coding practices.

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -6,3 +6,5 @@ alias npmd="npm run dev"
 alias vim=nvim
 alias ls="exa --header --icons -la"
 alias nvim-config="nvim ~/.config/nvim/"
+alias cd="z"
+alias yd="yarn dev"
---
+++ linux/download-core-dependencies.sh
@@ -0,0 +1,5 @@
+# install nvm
+
+# install docker
+
+# install rust
